13cdc0862b698b13fce09227fc156bad98bc8936
Jessé Toledo <jesse_hacker@hotmail.com>
2021-08-29 02:54:54 -0300

    Connected to Mongo

---
+++ src/controllers/userController.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const User = require("../models/user");
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { name } = req.body;
+  const { email } = req.body;
+
+  try {
+    if (
+      await User.findOne({
+        email,
+      })
+    ) {
+      return res.status(400).send({
+        error: "User already exists",
+      });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+    });
+    user.password = undefined;
+
+    return res.send({
+      user,
+    });
+  } catch (err) {
+    return res.status(400).send({
+      error: "Registration failed",
+    });
+  }
+});
+
+module.exports = (app) => app.use("/auth", router);
--- src/database/index.js
+++ src/database/index.js
@@ -1,17 +1,16 @@
-require("dotenv").config();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const uri = 'mongodb://root:20DeJunhoDe1971@localhost:27017/'
+const uri = "mongodb://root:20DeJunhoDe1971@localhost:27017/";
 mongoose
-    .connect(uri)
-    .then(x => {
-        console.log(
-            `Connected to Mongo! Database name: "${x.connections[0].name}"`
-        );
-    })
-    .catch(err => {
-        console.error("Error connecting to mongo", err);
-    });
+  .connect(uri)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
+  });
 
 mongoose.Promise = global.Promise;
 
